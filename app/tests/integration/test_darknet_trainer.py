5e46c0c5203f993d130d93b86ea962b88941a12a
Zauberzeug on Edge Device <development@zauberzeug.com>
2021-10-25 10:06:14 +0200

    add integration test for converting points to boxes (bad readability)

--- app/tests/integration/test_darknet_trainer.py
+++ app/tests/integration/test_darknet_trainer.py
@@ -19,8 +19,8 @@ def web() -> Generator:
 @pytest.fixture(autouse=True, scope='module')
 def create_project():
     test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
-    project_configuration = {'project_name': 'pytest', 'inbox': 0, 'annotate': 0, 'review': 0, 'complete': 3, 'image_style': 'beautiful',
-                             'box_categories': 2, 'segmentation_categories': 2, 'thumbs': False, 'tags': 0, 'trainings': 1, 'box_detections': 3, 'box_annotations': 0}
+    project_configuration = {'project_name': 'pytest', 'inbox': 0, 'annotate': 0, 'review': 0, 'complete': 3, 'image_style': 'empty',
+                             'box_categories': 2, 'point_categories': 1, 'segmentation_categories': 2, 'thumbs': False, 'tags': 0, 'trainings': 1, 'box_detections': 3, 'box_annotations': 1, 'point_annotations': 1}
     assert test_helper.LiveServerSession().post(f"/api/zauberzeug/projects/generator",
                                                 json=project_configuration).status_code == 200
     yield
@@ -88,3 +88,38 @@ async def test_get_new_model():
     model_dot_weights_files = [path for path in files if 'model.weights' in path]
     assert len(model_dot_weights_files) == 1, "There must be a file named model.weights"
     assert os.path.isfile(model_dot_weights_files[0])
+
+
+@pytest.mark.asyncio
+async def test_point_is_added_when_training_starts():
+    model_id = await trainer_test_helper.assert_upload_model(
+        ['tests/integration/data/training.cfg', 'tests/integration/data/model.weights'],
+        'yolo'
+    )
+    darknet_trainer = darknet_test_helper.create_darknet_trainer()
+    context = Context(organization='zauberzeug', project='pytest')
+
+    await darknet_trainer.begin_training(context=context, source_model={'id': model_id})
+    await asyncio.sleep(1)
+    assert darknet_trainer.get_error() is None
+    assert 'CUDA-version' in darknet_trainer.get_log()
+
+    training_data = darknet_trainer.training.data
+    assert len(training_data.image_data) == 3
+    print(*training_data.categories, sep='\n')
+
+    assert len(training_data.categories) == 3, 'There should be 3 categories, 2 boxes and 1 point'
+
+    point_categories = [c for c in training_data.categories if c['type']
+                        == 'point']
+    assert len(point_categories) == 1, 'There should be one point category'
+    point_category = point_categories[0]
+
+    assert len(training_data.image_data[0]['point_annotations']) == 1
+    assert len(training_data.image_data[0]['box_annotations']
+               ) == 2, 'There should be two box_annotations. One original and one small_box converted from point'
+
+    assert training_data.image_data[0]['box_annotations'][1]['category_id'] == point_category['id']
+
+    darknet_trainer.stop_training()
+
